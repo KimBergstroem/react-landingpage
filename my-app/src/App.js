f2ecd1a0ceddb4e97503b49ff425bfa76aa662e7
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-19 19:33:57 +0000

    feat: added frontend components and css files included

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Signup from './components/Signup/Signup';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      
+      <>
+        <Navbar/>
+        <Hero/>
+        <Signup/>
+        <Footer/>
+      </>
+
     </div>
   );
 }
